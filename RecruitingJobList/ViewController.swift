3911f2a442856efefd021fef2bd623605a55639a
大石陽波 <yoha.oishi@bizreach.co.jp>
2019-09-16 03:01:32 +0900

    Initial Commit

---
+++ RecruitingJobList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecruitingJobList
+//
+//  Created by 大石 陽波 on 2019/09/16.
+//  Copyright © 2019 Yoha Oishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
